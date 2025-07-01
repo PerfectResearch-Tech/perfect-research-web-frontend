5b0c2437f32ee0e7eee5175835646e227128f609
ADODODJI Kokouvi Damaz <adododji7@gmail.com>
2025-07-01 13:03:48 +0000

    changement des textes et mockup sur l'interface d'acceuil

--- app/data/cta.ts
+++ app/data/cta.ts
@@ -1,11 +1,11 @@
 export const ctaDetails = {
-    heading: 'Join Over 1 Million Users To Transform Your Finances',
+    // heading: 'Join Over 1 Million Users To Transform Your Finances',
 
 //    heading: 'Rejoignez plus de 1 Million d\'utilisateurs pour transformer vos finances',
-// heading: "Rejoignez plus de 1 Million d'utilisateurs pour transformer vos finances",
+heading: "Rejoignez plus de 1 Million d'utilisateurs pour transformer vos Recherches",
 
 
-    subheading: 'Your journey to financial freedom starts here. Download Finwise today and take the first step towards a brighter financial future!',
+    subheading: 'Téléchargez notre plateforme dès aujourd’hui et franchissez la première étape vers une recherche plus rapide, plus intelligente et plus collaborative.',
     appStoreUrl: '#',
     googlePlayUrl: '#'
 }
