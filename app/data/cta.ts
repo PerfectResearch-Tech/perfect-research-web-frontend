99ae1489085883ece65825ee79347cf50ebed01d
ADODODJI Kokouvi Damaz <adododji7@gmail.com>
2025-07-01 16:54:15 +0000

    declaration des types dans cta

--- app/data/cta.ts
+++ app/data/cta.ts
@@ -1,14 +1,13 @@
-export const ctaDetails = {
-    // heading: 'Join Over 1 Million Users To Transform Your Finances',
+type CtaDetails = {
+  heading: string;
+  subheading: string;
+  appStoreUrl: string;
+  googlePlayUrl: string;
+};
 
-//    heading: 'Rejoignez plus de 1 Million d\'utilisateurs pour transformer vos finances',
-heading: "Rejoignez plus de 1 Million d'utilisateurs pour transformer vos Recherches",
-
-// heading: "Rejoignez notre communauté",
-
-
-
-    subheading: 'Téléchargez notre plateforme dès aujourd’hui et franchissez la première étape vers une recherche plus rapide, plus intelligente et plus collaborative.',
-    appStoreUrl: '#',
-    googlePlayUrl: '#'
-}
+export const ctaDetails: CtaDetails = {
+  heading: "Rejoignez plus de 1 Million d'utilisateurs pour transformer vos Recherches",
+  subheading: 'Téléchargez notre plateforme dès aujourd’hui et franchissez la première étape vers une recherche plus rapide, plus intelligente et plus collaborative.',
+  appStoreUrl: '#',
+  googlePlayUrl: '#'
+};
